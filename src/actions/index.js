a1cdb2277a48122cf95e97478f4fefd0e838f700
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2017-09-19 17:53:59 +0530

    Wait for response and navigate programatically

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,10 +15,15 @@ export function fetchPosts() {
   );
 }
 
-export function createPost(values) {
-  const request = axios.post(`${API_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+  const request = axios.post(`${API_URL}/posts${API_KEY}`, values).then(
+    () => callback()
+  );
+
   return(
-    type: CREATE_POST,
-    payload: request
+    {
+      type: CREATE_POST,
+      payload: request
+    }
   );
 }
