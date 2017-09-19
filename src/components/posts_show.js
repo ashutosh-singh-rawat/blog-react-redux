5f1d25814a1492f4966464a8d1a1a5e9247848ef
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2017-09-19 17:58:52 +0530

    Posts show component

---
+++ src/components/posts_show.js
@@ -0,0 +1,19 @@
+// import _ from 'lodash';
+import React,{Component} from 'react';
+// import {connect} from 'react-redux';
+// import { createPost } from '../actions';
+// import { Field, reduxForm } from 'redux-form';
+// import { Link } from  'react-router-dom';
+
+class PostsShow extends Component {
+  render(){
+    const { handleSubmit } = this.props;
+    return(
+      <div>
+        PostsShow
+      </div>
+    );
+  }
+}
+
+export default PostsShow;
