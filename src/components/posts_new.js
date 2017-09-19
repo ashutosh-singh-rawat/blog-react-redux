5b5d40be9b327a35c35171215733fd2da2b41969
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2017-09-19 13:19:36 +0530

    PostsNew component boilerplate

---
+++ src/components/posts_new.js
@@ -0,0 +1,16 @@
+// import _ from 'lodash';
+import React,{Component} from 'react';
+// import {connect} from 'react-redux';
+// import { fetchPosts } from '../actions';
+
+class PostsNew extends Component {
+  render(){
+    return(
+      <div>
+        PostsNew
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
